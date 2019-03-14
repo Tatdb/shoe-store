8fe2cc5fbc9b457abfc7fd07017b9e6182f0f766
Tatdb <tattz66@yahoo.com>
2019-03-14 06:28:10 -0700

    commit changes first

---
+++ src/data.js
@@ -0,0 +1,111 @@
+export const storeProducts = [
+  {
+    id: 1,
+    title: "Adidas NEO Men's Basketball Shoes",
+    img: "img/Adidas.jpg",
+    price: 49.95,
+    company: "Adidas",
+    info:
+      "Whether on the court or off the court, these Adidas NEO's are sure to fit. Made with Cloudfoam cushioning for ultimate comfort and a rubber sole, you'll be shooting hoops and making moves in no time!",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 2,
+    title: "Converse All-Stars Sneakers",
+    img: "img/Converse.jpg",
+    price: 44.95,
+    company: "Converse",
+    info:
+      "These Converse Chuck Taylors are sure to impress. Mix and Match with bold colors. Wear them to your favorite party and even formal event. Made by Converse, a subsidiary of Nike Inc., you are sure to wear quality and be functional yet fashionable.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 3,
+    title: "CMM Men's Luxury Loafer Shoes",
+    img: "img/Dancing.jpg",
+    price: 75.95,
+    company: "CMM",
+    info:
+      "Get these party shoes for your next big celebration! Made with ultra-soft suede and maching gold pendants, you'll be a showstopper and life of the party (dare we say) in these. Get this effortless slip-on style today!",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 4,
+    title: "Nike Sportswear Shoes",
+    img: "img/Nike-Shoes.jpg",
+    price: 85.95,
+    company: "Nike",
+    info:
+      "Nike never fails to impress with quality, function and flawless fashion. Made to deliver comfort and style, your feet will thank you. Whether hiking, playing tennis or jogging, you will be satisfied after an intense activity and happy feet.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 5,
+    title: "Formal Business Leather Oxford Shoes",
+    img: "img/Oxford.jpg",
+    price: 59.95,
+    company: "Grimetin",
+    info:
+      "Satisfy your taste for formality and a little bit of flair with these Oxford Men's Shoes. Made from genuine leather splashed with a touch of shine, these Oxfords will be the highlight of your outfit.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 6,
+    title: "Rehab Light Grey Men's Sneaker",
+    img: "img/rehab-nazar-light-grey.jpg",
+    price: 89.95,
+    company: "Rehab",
+    info:
+      "These Rehab high-top light grey men's sneakers are made with nubuck (high quality washed). Wear these to a Business-casual event or Friday night party for a sure compliment and boost to confidence. Dress down or up in this fine men's light grey sneaker.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 7,
+    title: "Adidas Men's Fashion Sneaker",
+    img: "img/Tennis.jpg",
+    price: 59.95,
+    company: "Adidas",
+    info:
+      "Adidas delives function without compromising form. These great quality sneakers are for the fitness and couch enthusiasts both. Made with durable materials for quality and longevity, there is no going wrong with securing one of these in your closet!",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 8,
+    title: "NoBull Men's Training Shoes",
+    img: "img/Training.jpg",
+    price: 94.95,
+    company: "Nobull",
+    info:
+      "Gym enthusiasts and fitness novices alike can thrive in these Nobull training shoes. Jogging, Lifting, Gliding and Summersaulting are all accommodated for in these comfy and well designed training shoes. Made for multi-environment usage, a guaranteed fit will leave you wanting more out of fitness than before!",
+    inCart: false,
+    count: 0,
+    total: 0
+  }
+];
+
+export const detailProduct = {
+  id: 5,
+  title: "Formal Business Leather Oxford Shoes",
+  img: "img/Oxford.jpg",
+  price: 59.95,
+  company: "Grimetin",
+  info:
+    "Satisfy your taste for formality and a little bit of flair with these Oxford Men's Shoes. Made from genuine leather splashed with a touch of shine, these Oxfords will be the highlight of your outfit.",
+  inCart: false,
+  count: 0,
+  total: 0
+};
